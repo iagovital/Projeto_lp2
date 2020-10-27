cd7c46e9c42a5598021d40ac0da08e8d0c53ee6e
iagovital <iago.vital@academico.ifpb.edu.br>
2020-10-26 21:02:43 -0300

    Projeto 2

--- Node/public/temp.js
+++ Node/public/temp.js
@@ -1,74 +1,76 @@
 class conversao {
-	constructor(Farenheit, Celsius, Kelvin){
-		this.Farenheit = Farenheit;
-		this.Celsius = Celsius;
-		this.Kelvin = Kelvin;
+	constructor(Farenheit, Celsius, Kelvin) {
+	  this.Farenheit = Farenheit;
+	  this.Celsius = Celsius;
+	  this.Kelvin = Kelvin;
 	}
-}
-class enviar {
-	constructor(url){
-		this.url =url;
+  }
+  class enviar {
+	constructor(url) {
+	  this.url = url;
 	}
-	inserir(Farenheit, Celsius, Kelvin){
-		return fetch(this.url, {
-			method: post,
-			body: JSON.stringify({Farenheit, Celsius, Kelvin}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}).then(resposta => resposta.json())
+	inserir(Farenheit, Celsius, Kelvin) {
+	  return fetch(this.url, {
+		method: "POST",
+		body: JSON.stringify(Farenheit, Celsius, Kelvin),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	  }).then((resposta) => resposta.json());
 	}
-}
-
-const Farenheit;
-const Celsius;
-const Kelvin;
-
-function convertTemp(direction)
-{
- //instanciando objetos
-var fObj = document.convert.ftemp, cObj = document.convert.ctemp, kObj = document.convert.ktemp;
-
-//definindo direções para conversões
-if (direction == "f") 
-{
-	Farenheit = fObj;
-	cObj.value = Math.round(((fObj.value -32)/9)*5);
-	Celsius = cObj;
-	kObj.value = Math.round((parseFloat(cObj.value) + 273));
-	Kelvin = kObj;
-	let a = new conversao(Farenheit, Celsius, Kelvin);
-	let b = new enviar("https://localhost:3000/enviar");
-	b.inserir(a).then(resposta=>{console.log(resposta)});
-
-} 
-else if (direction == "k") 
- {
-	Kelvin = kObj;
-	cObj.value = Math.round(kObj.value - 273);
-	Celsius = cObj;
-	fObj.value = Math.round((parseFloat(cObj.value) * (9/5) +32));
-	Farenheit = fObj;
-	let a = new conversao(Farenheit, Celsius, Kelvin);
-	let b = new enviar("https://localhost:3000/enviar");
-	b.inserir(a).then(resposta=>{console.log(resposta)});
- }
-else
- {
-	Celsius = cObj;
-	fObj.value = Math.round((parseFloat(cObj.value) * (9/5)) + 32);
-	Farenheit = fObj;
-	kObj.value = Math.round((parseFloat(cObj.value) + 273));
-	Kelvin = kObj;
-	let a = new conversao(Farenheit, Celsius, Kelvin);
-	let b = new enviar("https://localhost:3000/enviar");
-	b.inserir(a).then(resposta=>{console.log(resposta)});
+  }
+  
+  let Farenheit;
+  let Celsius;
+  let Kelvin;
+  
+  function convertTemp(direction) {
+	//instanciando objetos
+	var fObj = document.convert.ftemp,
+	  cObj = document.convert.ctemp,
+	  kObj = document.convert.ktemp;
+  
+	//definindo direções para conversões
+	if (direction == "f") {
+	  Farenheit = fObj.value;
+	  cObj.value = Math.round(((fObj.value - 32) / 9) * 5);
+	  Celsius = cObj.value;
+	  console.log();
+	  kObj.value = Math.round(parseFloat(cObj.value) + 273);
+	  Kelvin = kObj.value;
+	  let a = new conversao(Farenheit, Celsius, Kelvin);
+	  console.log(Farenheit, Celsius, Kelvin);
+	  let b = new enviar("https://4ceoi.sse.codesandbox.io/enviar");
+	  b.inserir(a).then((resposta) => {
+		console.log(resposta);
+	  });
+	} else if (direction == "k") {
+	  Kelvin = kObj.value;
+	  cObj.value = Math.round(kObj.value - 273);
+	  Celsius = cObj.value;
+	  fObj.value = Math.round(parseFloat(cObj.value) * (9 / 5) + 32);
+	  Farenheit = fObj.value;
+	  let a = new conversao(Farenheit, Celsius, Kelvin);
+	  let b = new enviar("https://4ceoi.sse.codesandbox.io/enviar");
+	  b.inserir(a).then((resposta) => {
+		console.log(resposta);
+	  });
+	} else {
+	  Celsius = cObj.value;
+	  fObj.value = Math.round(parseFloat(cObj.value) * (9 / 5) + 32);
+	  Farenheit = fObj.value;
+	  kObj.value = Math.round(parseFloat(cObj.value) + 273);
+	  Kelvin = kObj.value;
+	  let a = new conversao(Farenheit, Celsius, Kelvin);
+	  let b = new enviar("https://4ceoi.sse.codesandbox.io/enviar");
+	  b.inserir(a).then((resposta) => {
+		console.log(resposta);
+	  });
 	}
-}
-
-function clearAll()
-{
-document.convert.ftemp.value="";
-document.convert.ctemp.value="";
-document.convert.ktemp.value="";
-}
+  }
+  function clearAll() {
+	document.convert.ftemp.value = "";
+	document.convert.ctemp.value = "";
+	document.convert.ktemp.value = "";
+  }
+  
